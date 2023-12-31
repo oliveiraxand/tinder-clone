368cc8cff2c0e370a59e3616df59b1e9f457e6c4
oliveiraxand <alexandre.osantos253@gmail.com>
2023-12-31 11:55:17 -0300

    feat: onbarding page

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,6 +1,192 @@
+import { useState } from 'react';
+import Nav from '../components/Nav';
+
 const Onboarding = () => {
+
+  const [formData, setFormData] = useState({
+    user_id: "",
+    first_name:"",
+    dob_day: "",
+    dob_month: "",
+    dob_year: "",
+    show_gender: false,
+    gender_identity: 'man',
+    gender_interest: 'woman',
+    email: '',
+    url: '',
+    about: '',
+    matches: []
+  })
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+
+    console.log(formData)
+  }
   return (
-    <div>Onboarding</div>
+    <>
+      <Nav
+        minimal={true}
+        setShowModel={() => { }}
+        showModel={false}
+      />
+      <div className="onboarding">
+        <h2>CREATE ACCOUNT</h2>
+
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label htmlFor='first_name'>First Name</label>
+            <input
+              id='first_name'
+              type='text'
+              name='first_name'
+              placeholder='First Name'
+              required={true}
+              value={formData.first_name}
+              onChange={handleChange}
+            />
+            <label>Birthday</label>
+            <input
+              id='dob_day'
+              type='text'
+              name='dob_day'
+              placeholder='DD'
+              required={true}
+              value={formData.dob_day}
+              onChange={handleChange}
+            />
+            <input
+              id='dob_month'
+              type='text'
+              name='dob_month'
+              placeholder='MM'
+              required={true}
+              value={formData.dob_month}
+              onChange={handleChange}
+            />
+            <input
+              id='dob_year'
+              type='text'
+              name='dob_year'
+              placeholder='YYYY'
+              required={true}
+              value={formData.dob_year}
+              onChange={handleChange}
+            />
+
+            <label>Gender</label>
+            <div className='multiple-input-container'>
+              <input
+                id='man-gender-identity'
+                type='radio'
+                name='gender_identity'
+                required={true}
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_identity === 'man'}
+              />
+              <label htmlFor='man-gender-identity'>Man</label>
+              <input
+                id='woman-gender-identity'
+                type='radio'
+                name='gender_identity'
+                required={true}
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_identity === 'woman'}
+              />
+              <label htmlFor='woman-gender-identity'>Woman</label>
+              <input
+                id='more-gender-identity'
+                type='radio'
+                name='gender_identity'
+                required={true}
+                value="more"
+                onChange={handleChange}
+                checked={formData.gender_identity === 'more'}
+              />
+              <label htmlFor='more-gender-identity'>More</label>
+            </div>
+            <label htmlFor='show-gender'>Show gender on my profile</label>
+            <input
+              id='show-gender'
+              type='checkbox'
+              name='show_gender'
+              onChange={handleChange}
+              checked={formData.show_gender}
+            />
+
+            <label>Show me</label>
+            <div className='multiple-input-container'>
+              <input
+                id='man-gender-interest'
+                type='radio'
+                name='gender_interest'
+                required={true}
+                value="man"
+                onChange={handleChange}
+                checked={formData.gender_interest === 'man'}
+              />
+              <label htmlFor='man-gender-interest'>Man</label>
+              <input
+                id='woman-gender-interest'
+                type='radio'
+                name='gender_interest'
+                required={true}
+                value="woman"
+                onChange={handleChange}
+                checked={formData.gender_interest === 'woman'}
+              />
+              <label htmlFor='woman-gender-interest'>Woman</label>
+              <input
+                id='more-gender-interest'
+                type='radio'
+                name='gender_interest'
+                required={true}
+                value="more"
+                onChange={handleChange}
+                checked={formData.gender_interest === 'more'}
+              />
+              <label htmlFor='more-gender-interest'>Everyone</label>
+            </div>
+            <label htmlFor="about">About me</label>
+            <input
+              id="about"
+              type="text"
+              name="about"
+              required={true}
+              placeholder="I like long walks..."
+              value={formData.about}
+              onChange={handleChange}
+            />
+            <input type="submit" />
+          </section>
+          <section>
+            <lable htmlFor="">Profile Photo</lable>
+            <input
+              type="url"
+              name="url"
+              id="url"
+              onChange={handleChange}
+              required={true}
+            />
+            <div className='photo-container'>
+              <img src={formData.url} alt="Profile pic preview"/>
+            </div>
+          </section>
+        </form>
+      </div>
+    </>
   )
 }
 
